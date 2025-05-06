5ec5870acdf4c8922c2a823cdf8e29d13683d651
abinjustinkumaravel <abinjustinkumaravel@gmail.com>
2025-05-06 17:59:07 +0530

    hot-fix

--- src/utils/action.js
+++ src/utils/action.js
@@ -1,7 +1,7 @@
 //const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export async function uploadFile(file, setUploadStatus) {
-  //console.log(API_URL);
+//console.log(API_URL);
   if (!file) {
     setUploadStatus("Please select a file to upload.");
     return;
@@ -12,7 +12,7 @@ export async function uploadFile(file, setUploadStatus) {
 
   try {
     setUploadStatus("Uploading...");
-    const response = await fetch("/upload", {
+    const response = await fetch(`/upload`, {
       method: "POST",
       body: formData,
       headers: {
@@ -61,9 +61,10 @@ export async function chat(query) {
   }
   try {
     // setUploadStatus('Uploading...');
-    const response = await fetch("/chat",{
+    const response = await fetch(`/chat`, {
       method: "POST",
       headers: {
+        "Content-Type": "application/json",
         Accept: "application/json",
       },
       body: JSON.stringify({
